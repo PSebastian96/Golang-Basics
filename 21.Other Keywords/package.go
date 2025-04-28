bae60a684e8504325d6cf7e8b97da9eec2333fa3
PSebastian96 <123810890+PSebastian96@users.noreply.github.com>
2025-04-28 17:33:47 +0100

    Create package.go

---
+++ 21.Other Keywords/package.go
@@ -0,0 +1,15 @@
+// This is the main package and entry point of the program
+package main
+
+import "fmt"
+
+/*
+The `package` keyword defines the package a file belongs to.
+It’s the foundation for organizing your code into logical units.
+By convention, the main package is used for the entry point of a Go program.
+*/
+
+func main() {
+	// Print a message to the console
+	fmt.Println("This is the main package!")
+}
